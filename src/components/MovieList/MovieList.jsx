f47992489a11467afaf7bb626ad5506f3334c7bd
dimon <80713na@gmail.com>
2025-09-01 18:56:09 +0300

    Movie-list

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,14 +1,10 @@
 import { MovieCard } from '../MovieCard/MovieCard';
 import './MovieList.scss';
 
-
-export const MovieList =({movies}) => (
-<div className="movies">
-  {movies.map((film) => (
-  <MovieCard 
-  movie ={film}
-  key={film.imdbId}
-  />
-  ))}
-</div>
-)
+export const MovieList = ({ movies }) => (
+  <div className="movies">
+    { movies.map(film => (
+      <MovieCard movie = { film } key = { film.imdbId } />
+    ))}
+  </div>
+);
